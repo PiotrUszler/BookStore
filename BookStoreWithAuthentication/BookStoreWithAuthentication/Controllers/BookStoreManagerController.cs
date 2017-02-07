d627cba89c1f3376ec7acedfd10bf189af1aa0b0
Piotr Uszler <xothas@gmail.com>
2017-02-07 18:34:13 +0100

    Fixed book editting

--- BookStoreWithAuthentication/BookStoreWithAuthentication/Controllers/BookStoreManagerController.cs
+++ BookStoreWithAuthentication/BookStoreWithAuthentication/Controllers/BookStoreManagerController.cs
@@ -2,6 +2,9 @@
 using BookStoreWithAuthentication.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -97,48 +100,70 @@ namespace BookStoreWithAuthentication.Controllers
             return View(book);
         }
 
-        //POST: /BookStoreManager/EditBook/5
         [HttpPost]
-        public ActionResult EditBook([Bind(Include = "ID, Title, NumOfPages, Price, Authors, CategoryID, SeriesID, PublisherID")]Book book, string author)
+        public ActionResult EditBook(int? id, string author)
         {
-            //TODO fix updating authors, it doesnt work
-            if (ModelState.IsValid)
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var bookToUpdate = db.Books.Include(b => b.Authors).Where(i => i.ID == id).Single();
+            if(TryUpdateModel(bookToUpdate, "", new string[] { "Title", "NumOfPages", "Price", "CategoryID", "SeriesID", "PublisherID" }))
+            {
+                try
+                {
+                    
+                    UpdateBookAuthors(author, bookToUpdate);
+                    UpdateBookCover(bookToUpdate, this.Request);
+                    
+
+                    db.Entry(bookToUpdate).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                } catch(RetryLimitExceededException e)
+                {
+                    ModelState.AddModelError("", e);
+                }
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", bookToUpdate.CategoryID);
+            ViewBag.SeriesID = new SelectList(db.Series, "ID", "Name", bookToUpdate.SeriesID);
+            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", bookToUpdate.PublisherID);
+            ViewBag.Authors = bookToUpdate.AuthorsToString();
+
+            return View(bookToUpdate);
+
+        }
+        #endregion
+
+        private void UpdateBookAuthors(string author, Book bookToUpdate)
+        {
+            List<Author> authors = new List<Author>();
+            if (author != null)
             {
                 string[] auths = author.Split(',');
-                List<Author> authors = new List<Author>();
                 foreach (var item in auths)
                 {
                     Author auth = db.Authors.SingleOrDefault(a => (a.FirstName + " " + a.LastName) == item);
                     authors.Add(auth);
                 }
-                book.Authors = authors;
-                
-                //File upload, add some sort of waiting to complete uploading
-                if (Request.Files.Count > 0)
-                {
-                    var file = Request.Files[0];
-
-                    if (file != null && file.ContentLength > 0)
-                    {
-                        var fileName = Path.GetFileName(file.FileName);
-                        var path = Path.Combine(Server.MapPath("~/Content/Images/"), fileName);
-                        file.SaveAs(path);
-                        book.ImagePath = fileName;
-                    }
-                }
-                db.Entry(book).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
             }
-
-            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", book.CategoryID);
-            ViewBag.SeriesID = new SelectList(db.Series, "ID", "Name", book.SeriesID);
-            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", book.PublisherID);
-            ViewBag.Authors = book.AuthorsToString();
-
-            return View(book);
+            bookToUpdate.Authors = authors;
         }
 
-        #endregion
+        private void UpdateBookCover(Book bookToUpdate, HttpRequestBase request)
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/Images/"), fileName);
+                    file.SaveAs(path);
+                    bookToUpdate.ImagePath = fileName;
+                }
+            }
+        }
     }
-}
+    }
